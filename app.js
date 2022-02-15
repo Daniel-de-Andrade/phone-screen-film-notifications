180d6481ec2b86f5be4f3268dd713aab646d40eb
Andre Oliveira <andre@publistar.com.br>
2022-02-14 22:02:23 -0300

    Default 404 route added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 const usersRoute = require('./routes/users')
 
+app.use(morgan('dev'))
+
 app.use('/users', usersRoute)
+
+app.use((req, res) => {
+    res.status(404).send({
+        message : "Page not found."
+    })
+})
 
 module.exports = app
